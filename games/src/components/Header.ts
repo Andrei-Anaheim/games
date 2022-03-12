52d6551e6b8a95f77bc6539078d49f03ae8a4590
Andrei-Anaheim <86974515+Andrei-Anaheim@users.noreply.github.com>
2022-03-12 23:33:03 +0300

    feat: add game deviatka

---
+++ games/src/components/Header.ts
@@ -0,0 +1,79 @@
+import Component from './common/Component';
+import Drawer from './drawer/Drawer';
+import Button from './common/Button';
+import AuthorizationForm from './common/AuthorizationForm';
+import '../scss/layout/_header.scss';
+
+class Header implements Component {
+  private class: string;
+
+  public constructor(options) {
+    this.class = options.class;
+  }
+
+  public async render(): Promise<string> {
+    const authorizationForm = await Drawer.drawComponent(AuthorizationForm, {
+      class: 'login',
+      id: 'authorization-form',
+    });
+
+    const authorizationButton = await Drawer.drawComponent(Button, {
+      id: 'authorization-button',
+      class: 'header__button',
+      text: `${AuthorizationForm.isAuthorized ? 'Log out' : 'Log in'}`,
+    });
+    const view = `
+    <div class="wrapper header__wrapper ${this.class ? this.class : ''}">
+      <a href="/#" class="instead-of-logo">A.T.</a>
+      <nav class="header__navigation">
+        <ul class="navigation">
+          <li>
+            <a href="/#/nine/start" class="link navigation__link" id="nine">Nine</a>
+          </li>
+          <!--<li>
+            <a href="/#/audio_challenge" class="link navigation__link" id="audio_challenge"
+              >Audio challenge</a
+            >
+          </li>
+          <li><a href="/#/sprint" class="link navigation__link" id="sprint">Sprint</a></li> -->
+        </ul>
+      </nav>
+      ${authorizationButton}
+    </div>
+    ${authorizationForm}
+    `;
+    return view;
+  }
+
+  private showActiveMenuItem(): void {
+    const id: string = window.location.hash.slice(2).toLowerCase();
+    const menuItems = document.querySelectorAll('.navigation__link') as NodeListOf<HTMLAnchorElement>;
+    menuItems.forEach((item) => item.classList.remove('active'));
+    const menuItem = document.getElementById(id) as HTMLElement;
+    if (menuItem) {
+      menuItem.classList.add('active');
+    }
+  }
+
+  public async after_render(): Promise<void> {
+    this.showActiveMenuItem();
+    window.addEventListener('hashchange', this.showActiveMenuItem);
+
+    const button = document.getElementById('authorization-button') as HTMLElement;
+    const form = document.getElementById('authorization-form') as HTMLElement;
+    const inputs = document.querySelectorAll('.login__form input') as NodeListOf<HTMLInputElement>;
+
+    button.addEventListener('click', () => {
+      if (AuthorizationForm.isAuthorized) {
+        AuthorizationForm.isAuthorized = false;
+        inputs.forEach((input) => (input.value = ''));
+        localStorage.clear();
+        button.innerHTML = 'Log in';
+      } else {
+        form.classList.add('active');
+      }
+    });
+  }
+}
+
+export default Header;
---
+++ games/src/components/router/Router.ts
@@ -0,0 +1,56 @@
+import Utils from '../../services/Utils';
+import Drawer from '../drawer/Drawer';
+import Page from '../pages/Page';
+import Error404 from '../pages/Error404';
+import Home from '../pages/Home';
+import Nine from '../pages/Nine';
+import Header from '../Header';
+import Footer from '../Footer';
+
+export interface Request {
+  resource: string;
+  id: string;
+  verb: string;
+}
+
+const routes: Record<string, Page> = {
+  '/': new Home(),
+  '/nine/:id': new Nine(),
+};
+const errorPage: Error404 = new Error404();
+
+class Router {
+  private;
+  private async router(): Promise<void> {
+    // Get the parsed URl from the addressbar
+    const request: Request = Utils.parseRequestURL();
+
+    const parsedURL: string =
+      (request.resource ? `/${request.resource}` : '/') +
+      (request.id ? '/:id' : '') +
+      (request.verb ? '/:verb' : '');
+
+    // Get the page from our hash of supported routes.
+    // If the parsed URL is not in our list of supported routes, select the 404 page instead
+    const page: Page = routes[parsedURL] ? routes[parsedURL] : errorPage;
+    Drawer.drawPage(page);
+  }
+
+  private async renderHeaderAndFooter(): Promise<void> {
+    const header = await Drawer.drawComponent(Header);
+    (document.getElementById('header_container') as HTMLElement).innerHTML = header;
+    const footer = await Drawer.drawComponent(Footer);
+    (document.getElementById('footer_container') as HTMLElement).innerHTML = footer;
+  }
+
+  public init(): void {
+    // Listen on hash change:
+    window.addEventListener('hashchange', this.router);
+
+    // Listen on page load:
+    window.addEventListener('load', this.renderHeaderAndFooter);
+    window.addEventListener('load', this.router);
+  }
+}
+
+export default Router;
